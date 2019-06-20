950b70e702c16e2b173fe8e30cfce79a4bdaa5fc
Ashutoshjhavanderlande <ashutosh.jha@vanderlande.com>
2019-06-20 14:03:57 +0530

    Exception Second commit

--- ExceptionHandling/ExceptionHandling/Program.cs
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -7,10 +7,24 @@ namespace ExceptionHandling
     {
         static void Main(string[] args)
         {
-            StreamReader sr = new StreamReader(@"C:\New folder\log.txt");
-            Console.WriteLine(sr.ReadToEnd());
-            sr.Close();
-            Console.ReadLine();
+            StreamReader sr = null;
+            try
+            {
+                sr = new StreamReader(@"C:\New folder\log.txt");
+                Console.WriteLine(sr.ReadToEnd());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (sr != null)
+                {
+                    sr.Close();
+                }
+                Console.WriteLine("Finally block.....");
+            }
         }
     }
 }
