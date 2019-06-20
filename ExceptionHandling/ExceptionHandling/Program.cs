a0587144bf05c7b64d9556684d33a29b47de2de8
Ashutoshjhavanderlande <ashutosh.jha@vanderlande.com>
2019-06-20 13:54:08 +0530

    Exception Handling

---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace ExceptionHandling
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"C:\New folder\log.txt");
+            Console.WriteLine(sr.ReadToEnd());
+            sr.Close();
+            Console.ReadLine();
+        }
+    }
+}
