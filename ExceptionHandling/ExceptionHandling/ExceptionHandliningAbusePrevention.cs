e3cf935b2b7dd85d589f0733362289b2596bdb27
Ashutoshjhavanderlande <ashutosh.jha@vanderlande.com>
2019-06-20 19:39:21 +0530

    Exception Handling abuse Prevention commit

--- ExceptionHandling/ExceptionHandling/ExceptionHandliningAbusePrevention.cs
+++ ExceptionHandling/ExceptionHandling/ExceptionHandliningAbusePrevention.cs
@@ -8,5 +8,46 @@ namespace ExceptionHandling
 {
     public class ExceptionHandliningAbusePrevention
     {
+        public void AbusePrevention()
+        {
+            int numerator, denomurator, result;
+            bool IsNumeratorConvertable;
+            bool IsDenomratorConvertable ;
+            try
+            {
+                Console.WriteLine("Enter Numerator:");
+                IsNumeratorConvertable = int.TryParse(Console.ReadLine(), out numerator);
+                if (IsNumeratorConvertable)
+                {
+                    Console.WriteLine("Enter Denomurator:");
+                    IsDenomratorConvertable = int.TryParse(Console.ReadLine(), out denomurator);
+                    if (IsDenomratorConvertable && denomurator != 0)
+                    {
+                        result = numerator / denomurator;
+                        Console.WriteLine("result ={0}", result);
+                    }
+                    else
+                    {
+                        if (denomurator == 0)
+                        {
+                            Console.WriteLine("Denomerator should not be 0");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Denomerator should be between {0} and {1}",int.MinValue,int.MaxValue);
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Numerator should be between {0} and {1}", int.MinValue, int.MaxValue);
+                    //Console.WriteLine("Only Numbers are allowed..");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Some other exception occurs {0}",ex);
+            }
+        }
     }
 }
