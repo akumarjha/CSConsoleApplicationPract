e544518caeb755f34c6e38981ec8eb53b504f234
Ashutoshjhavanderlande <ashutosh.jha@vanderlande.com>
2019-06-21 11:49:21 +0530

    Enums GetNames & GetValues

--- EnumDemo/EnumDemo/EnumsSolved.cs
+++ EnumDemo/EnumDemo/EnumsSolved.cs
@@ -10,6 +10,17 @@ namespace EnumDemo
     {
         static void Main(string[] args)
         {
+            string[] names = Enum.GetNames(typeof(Gender2));
+            foreach (string name in names)
+            {
+                Console.WriteLine("Name is : {0}", name);
+            }
+            Gender2[] values = (Gender2[])Enum.GetValues(typeof(Gender2));
+            foreach (int value in values)
+            {
+                Console.WriteLine("Value is : {0}", value);
+            }
+
             Customer1[] customers = new Customer1[3];
             customers[0] = new Customer1()
             {
@@ -52,6 +63,18 @@ namespace EnumDemo
         Unknown,
         Male,
         Female
+    }
+    public enum Gender1 :short // by default Type of enum is Int but we can change it 
+    {
+        Unknown =1, // by default value starts from 0 but but we can start from anywhere within the range of Short
+        Male, //2
+        Female //3
+    }
+    public enum Gender2 : short // by default Type of enum is Int but we can change it 
+    {
+        Unknown = 1, // by default value starts from 0 but but we can start from anywhere within the range of Short
+        Male =10, //2
+        Female =50 //3
     }
     public class Customer1
     {
